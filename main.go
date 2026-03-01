25a2f8b3c612e2233a1446cc949547cb4977e56f
kaiotcp1 <kaiotcp1@gmail.com>
2026-03-01 14:17:21 -0300

    initialize go module and add basic structure with main, entities, and dependencies

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := gin.Default()
+
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, Gin!"})
+	})
+
+	app.Run(":8080")
+}
