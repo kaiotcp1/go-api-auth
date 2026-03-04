9d9e6692caf9f9c4dbc4dd3e4475b6440df3f338
kaiotcp1 <kaiotcp1@gmail.com>
2026-03-04 20:17:03 -0300

    feat: add env config for access database

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"go-api/src/repositories"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("arquivo .env não encontrado, usando variáveis do ambiente")
+	}
+
+	uri := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("MONGO_DB_NAME")
+	userCollection := os.Getenv("MONGO_USER_COLLECTION")
+	taskCollection := os.Getenv("MONGO_TASK_COLLECTION")
+
+	repoUser, errUser := repositories.NewUserRepository(uri, dbName, userCollection)
+	repoTask, errTask := repositories.NewTaskRepository(uri, dbName, taskCollection)
 	app := gin.Default()
 
 	app.GET("/", func(c *gin.Context) {
